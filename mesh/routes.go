16233b0148308ec93072f67e733437e6c516444f
cwallsfdc <cwall@salesforce.com>
2024-09-26 12:55:06 -0600

    Fix body to fwd request; append URI to fwd request

--- mesh/routes.go
+++ mesh/routes.go
@@ -42,7 +42,7 @@ func NewRoutes() *Routes {
 }
 
 func getForwardUrl(r *http.Request, appPort string) (string, error) {
-	url := fmt.Sprintf("http://127.0.0.1:%s%s", appPort, r.URL.Path)
+	url := fmt.Sprintf("http://127.0.0.1:%s%s", appPort, r.URL.RequestURI())
 	return url, nil
 }
 
@@ -57,26 +57,26 @@ func getForwardUrl(r *http.Request, appPort string) (string, error) {
 // If validation and authentication are successful, the mesh forwards the request
 // to the target app API.
 func (routes *Routes) Pass() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(incomingRespWriter http.ResponseWriter, incomingReq *http.Request) {
 		config := conf.GetConfig()
 		should_auth_disable := os.Getenv("HEROKU_INTEGRATION_SERVICE_MESH_AUTH_DISABLE")
 		bypass_auth, err := strconv.ParseBool(should_auth_disable)
 
 		// Get requestID from heahder; if not found, generate and set
-		requestID := r.Header.Get(HdrNameRequestID)
+		requestID := incomingReq.Header.Get(HdrNameRequestID)
 		if requestID == "" {
 			requestID = uuid.New().String()
-			w.Header().Set(HdrNameRequestID, requestID)
+			incomingRespWriter.Header().Set(HdrNameRequestID, requestID)
 			logWarn(requestID, HdrNameRequestID+" not found! Generated and set "+requestID)
 		}
 		defer timeTrack(requestID, time.Now(), "Heroku Integration Service Mesh")
 
 		// Log request
-		apiPath := r.URL.Path
+		apiPath := incomingReq.URL.Path
 		logInfo(requestID, "Processing request to API "+apiPath+"...")
 
 		// Validate request headers
-		requestHeader, err := ValidateRequest(requestID, r.Header)
+		requestHeader, err := ValidateRequest(requestID, incomingReq.Header)
 		if err != nil {
 			httpStatusCode := http.StatusUnauthorized
 			switch err.(type) {
@@ -85,7 +85,15 @@ func (routes *Routes) Pass() http.HandlerFunc {
 			default:
 			}
 			logError(requestID, err.Error())
-			http.Error(w, err.Error(), httpStatusCode)
+			http.Error(incomingRespWriter, err.Error(), httpStatusCode)
+			return
+		}
+
+		// Get the request body from the incoming request
+		incomingReqBody, err := io.ReadAll(incomingReq.Body)
+		if err != nil {
+			logError(requestID, "Error parsing body from the request: "+err.Error())
+			http.Error(incomingRespWriter, err.Error(), http.StatusForbidden)
 			return
 		}
 
@@ -112,7 +120,7 @@ func (routes *Routes) Pass() http.HandlerFunc {
 				status, err := callSalesforceAuth(requestID, authRequestBody, config.IntegrationUrl, config.InvocationToken)
 				if err != nil {
 					logError(requestID, "Error authorizing Salesforce request: "+err.Error())
-					http.Error(w, err.Error(), http.StatusUnauthorized)
+					http.Error(incomingRespWriter, err.Error(), http.StatusUnauthorized)
 					return
 				}
 				finalStatus = status
@@ -121,15 +129,7 @@ func (routes *Routes) Pass() http.HandlerFunc {
 				logInfo(requestID, "Found Data Action Target request")
 
 				// Get data from query params
-				queryParams := r.URL.Query()
-
-				// Get the request body from the initial request
-				bodyStr, err := io.ReadAll(r.Body)
-				if err != nil {
-					logError(requestID, "Error parsing body from the request: "+err.Error())
-					http.Error(w, err.Error(), http.StatusForbidden)
-					return
-				}
+				queryParams := incomingReq.URL.Query()
 
 				// Build Data Action Target authentication request Body
 				orgId = queryParams.Get(OrgIdQueryParm)
@@ -137,7 +137,7 @@ func (routes *Routes) Pass() http.HandlerFunc {
 					ApiName:   queryParams.Get(ApiNameQueryParam),
 					OrgID:     orgId,
 					Signature: requestHeader.XSignature,
-					Payload:   string(bodyStr),
+					Payload:   string(incomingReqBody),
 				}
 				unauthorizedMsg = "Org " + orgId + " not found or not connected to app and/or Data Action Target '" +
 					dataActionTargetAuthRequestBody.ApiName + "' signed key not found or is invalid"
@@ -146,7 +146,7 @@ func (routes *Routes) Pass() http.HandlerFunc {
 				status, err := callDataTargetActionAuth(requestID, dataActionTargetAuthRequestBody, config.IntegrationUrl)
 				if err != nil {
 					logError(requestID, "Error authenticating Data Action Target request: "+err.Error())
-					http.Error(w, err.Error(), status)
+					http.Error(incomingRespWriter, err.Error(), status)
 					return
 				}
 
@@ -162,49 +162,52 @@ func (routes *Routes) Pass() http.HandlerFunc {
 				} else {
 					logError(requestID, "Failed Integration authentication request: "+strconv.Itoa(finalStatus))
 				}
-				http.Error(w, http.StatusText(finalStatus), finalStatus)
-				w.WriteHeader(finalStatus)
+				http.Error(incomingRespWriter, http.StatusText(finalStatus), finalStatus)
+				incomingRespWriter.WriteHeader(finalStatus)
 				return
 			}
 		} else {
 			logWarn(requestID, "Bypassed authentication")
 		}
 
+		// Successful auth'd!
 		logInfo(requestID, "Authenticated request!")
 
-		// Successful auth'd, forward request to API
-		forwardApiUrl, err := getForwardUrl(r, config.AppPort)
+		// Forward request to target API
+		forwardApiUrl, err := getForwardUrl(incomingReq, config.AppPort)
 		logInfo(requestID, "Forwarding request to app API "+forwardApiUrl)
-		forwardReq, err := http.NewRequest(r.Method, forwardApiUrl, r.Body)
+		forwardReq, err := http.NewRequest(incomingReq.Method, forwardApiUrl, bytes.NewReader(incomingReqBody))
 		if err != nil {
 			logError(requestID, "Error assembling request: "+err.Error())
-			http.Error(w, err.Error(), http.StatusBadRequest)
+			http.Error(incomingRespWriter, err.Error(), http.StatusBadRequest)
 		}
 
 		// Apply request headers to forward request
-		for header, values := range r.Header {
+		for header, values := range incomingReq.Header {
 			for _, value := range values {
 				forwardReq.Header.Set(header, value)
 			}
 		}
 
+		// Forward request
 		client := &http.Client{}
-		resp, err := client.Do(forwardReq)
+		forwardResp, err := client.Do(forwardReq)
 		if err != nil {
 			logError(requestID, "Error forwarding request: "+err.Error())
-			http.Error(w, "Error forwarding request "+requestID, http.StatusBadGateway)
+			http.Error(incomingRespWriter, "Error forwarding request "+requestID, http.StatusBadGateway)
 		}
 
-		defer resp.Body.Close()
-
-		for header, values := range resp.Header {
+		// Copy incoming headers to forward request
+		for header, values := range forwardResp.Header {
 			for _, value := range values {
-				w.Header().Add(header, value)
+				incomingRespWriter.Header().Add(header, value)
 			}
 		}
 
-		w.WriteHeader(resp.StatusCode)
-		io.Copy(w, resp.Body)
+		// Copy forward request to incoming response
+		incomingRespWriter.WriteHeader(forwardResp.StatusCode)
+		io.Copy(incomingRespWriter, forwardResp.Body)
+		defer forwardResp.Body.Close()
 	}
 }
 
