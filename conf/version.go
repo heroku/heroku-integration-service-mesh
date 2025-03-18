1e7722a4cc2a05c8029a9011ef64d2d0336f0c0b
Marcel Santos <marcelsantos@salesforce.com>
2025-03-18 10:15:27 -0600

    Update patch version after bug fix

--- conf/version.go
+++ conf/version.go
@@ -2,4 +2,4 @@ package conf
 
 // VERSION is the app-global version string, which should be substituted with a
 // real value during build.  Follows semantic versioning: MAJOR.MINOR.PATCH
-var VERSION = "v0.1.1"
+var VERSION = "v0.1.2"
